3cb4d1945fa068ed17ec8352ecf0f2f05089bf78
Jack <stacksofjack@gmail.com>
2025-01-11 23:26:05 +0000

    firebase added

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDg_u29JmvlnirJHRwA-0wjHSnGiI9-rz0",
+  authDomain: "memetool-cfbe4.firebaseapp.com",
+  projectId: "memetool-cfbe4",
+  storageBucket: "memetool-cfbe4.firebasestorage.app",
+  messagingSenderId: "2287715906",
+  appId: "1:2287715906:web:39dfbfb76db328735e4fdb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
